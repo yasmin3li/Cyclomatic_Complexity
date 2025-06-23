3c2b736eb4506c783537db4650b4eaf3c1caa4cf
yasmin3li <yasmin123@gmail.com>
2025-06-22 19:19:53 -0700

    first commit

---
+++ LoanApp.Core/LoanEvaluator.cs
@@ -0,0 +1,45 @@
+﻿namespace LoanApp.Core;
+
+public class LoanEvaluator
+{
+    public string GetLoanEligibility(int income,
+     bool hasJob, int creditScore, int dependents, bool ownsHouse)
+    {
+        if (income < 2000)
+            return "Not Eligible";
+        
+        if (hasJob)
+        {
+            if (creditScore >= 700)
+            {
+                if (dependents == 0)
+                    return "Eligible";
+                else if (dependents <= 2)
+                    return "Review Manually";
+                else
+                    return "Not Eligible";
+            }
+            else if (creditScore >= 600)
+            {
+                if (ownsHouse)
+                    return "Review Manually";
+                else
+                    return "Not Eligible";
+            }
+            else
+            {
+                return "Not Eligible";
+            }
+        }
+        else
+        {
+            if (creditScore >= 750 && income > 5000 && ownsHouse)
+                return "Eligible";
+            else if (creditScore >= 650 && dependents == 0)
+                return "Review Manually";
+            else
+                return "Not Eligible";
+        }
+    }
+}
+//===========================================================================
---
+++ LoanApp.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace LoanApp.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
